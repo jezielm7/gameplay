99c008be1f8997a6c7e20066f37c89dd21585e00
jeziel <jezielmarques7@gmail.com>
2021-08-07 15:58:11 -0300

    update app theme

--- src/global/styles/theme.ts
+++ src/global/styles/theme.ts
@@ -1,7 +1,7 @@
 export const theme = {
   colors: {
     primary: '#E51C44',
-    
+
     secondary100: '#0D1033',
     secondary90: '#0D133D',
     secondary80: '#0D1647',
@@ -10,17 +10,17 @@ export const theme = {
     secondary50: '#243189',
     secondary40: '#1D2766',
     secondary30: '#495BCC',
-    
+
     overlay: 'rgba(0,0,0,0.7)',
     highlight: '#ABB1CC',
     heading: '#DDE3F0',
     line: '#991F36',
-    on: '#32BD50'
+    on: '#32BD50',
   },
   fonts: {
-    title700: 'Rajdhani_700Bold',
-    title500: 'Rajdhani_500Medium',
-    text400: 'Inter_400Regular',
-    text500: 'Inter_500Medium'
+    rajdhani700: 'Rajdhani_700Bold',
+    rajdhani500: 'Rajdhani_500Medium',
+    inter400: 'Inter_400Regular',
+    inter500: 'Inter_500Medium',
   }
-} 
+}
