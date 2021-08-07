f832c57b7cf69459ea5511ddb79576ca28265f9d
jeziel <jezielmarques7@gmail.com>
2021-08-07 15:59:22 -0300

    create background component

---
+++ src/components/Background/index.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+
+import { theme } from '../../global/styles/theme';
+
+import { Container } from './styles';
+
+interface BackgroundProps {
+  children: React.ReactNode;
+}
+
+function Background({ children }: BackgroundProps) {
+  const { secondary80, secondary100 } = theme.colors;
+
+  return (
+    <Container
+      colors={[secondary80, secondary100]}
+    >
+      {children}
+    </Container>
+  );
+};
+
+export default Background;
---
+++ src/components/Background/styles.ts
@@ -0,0 +1,8 @@
+import styled from 'styled-components/native';
+import { LinearGradient } from 'expo-linear-gradient';
+
+import { theme } from '../../global/styles/theme';
+
+export const Container = styled(LinearGradient)`
+  flex: 1;
+`;
