82caf21602f23a734faaf7d9c42004084dac6c80
jeziel <jezielmarques7@gmail.com>
2021-08-08 16:33:06 -0300

    lint

--- src/components/Background/index.tsx
+++ src/components/Background/index.tsx
@@ -12,9 +12,7 @@ function Background({ children }: BackgroundProps) {
   const { secondary80, secondary100 } = theme.colors;
 
   return (
-    <Container
-      colors={[secondary80, secondary100]}
-    >
+    <Container colors={[secondary80, secondary100]}>
       {children}
     </Container>
   );
