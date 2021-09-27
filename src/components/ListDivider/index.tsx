fd1ef1bfa3880637698a4f9cf6d25b94030ded02
jeziel <jezielmarques7@gmail.com>
2021-09-26 22:57:15 -0300

    create list divider component

---
+++ src/components/ListDivider/index.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+function ListDivider() {
+  return (
+    <Container />
+  );
+};
+
+export default ListDivider;
---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components/native';
+import { theme } from '../../global/styles/theme';
+
+export const Container = styled.View`
+  width: 80%;
+  height: 1px;
+  margin: 20px 0;
+  
+  align-self: flex-end;
+  background-color: ${theme.colors.secondary40};
+`;
